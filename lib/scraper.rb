4298bf0212f957f78c3365c966d024df8c16499f
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-12-10 18:56:49 -0500

    commit tree 7133a272af0bcf5387758e552866f3828f570b6b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css(".site-header__hero__headline").text.strip
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+    puts.course.text.strip
+end
